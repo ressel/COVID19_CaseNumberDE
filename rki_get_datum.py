895276843e68802de1713d69f91582934f7757ee
Christian Wienbruch <51226915+chriswien@users.noreply.github.com>
2021-02-22 09:11:24 +0100

    Update 2021-02-20

---
+++ rki_get_datum.py
@@ -0,0 +1,88 @@
+# Import libraries
+import requests
+import urllib.request
+import time
+import datetime
+from bs4 import BeautifulSoup
+
+# Set the URL you want to webscrape from
+#url = 'http://web.mta.info/developers/turnstile.html'
+url = 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html'
+
+# Connect to the URL
+response = requests.get(url)
+#print (response)
+
+# Parse HTML and save to BeautifulSoup object¶
+soup = BeautifulSoup(response.text, "html.parser")
+#print(soup)
+
+StandValue = soup.find("div", {"class":"dateOfIssue"})
+#print(StandValue)
+
+Stand = StandValue.find("p")
+#print(Stand)
+
+label = soup.find("p", text="Stand:")
+print(label.next_sibling.strip())
+
+#for text in Stand.strings:
+#    pass
+#print(text)
+
+#val=Stand.strings.split(':')[-1].strip()
+#print("VAl:", val)
+
+#print (soup.p["Stand:"])
+
+#rows = table.findAll('tr')
+#for row in rows:
+#    print(row)
+#
+#    cols = row.findAll('td')[6]
+#    print(cols)
+#
+#    country = cols[0].string
+#    number  = cols[1].string
+#    diff    = cols[2].string
+#    np100t  = cols[3].string
+#    dead    = cols[4].string
+#    #remarks = cols[5].string
+#
+#    #entry = (country, number, diff, np100t, dead, remarks)
+#    entry = (country, number, diff, np100t, dead)
+#    print (entry)
+
+#dt_date = datetime.datetime.now()
+#date    = datetime.date.today()
+##print ("The Current date is:" ,dt_date)
+##print("In specified format:", dt_date.isoformat())
+#filename='RKI_Corona_'+dt_date.isoformat()
+##this file contais all data from the RKI table
+#filename3='RKI_Corona_'+dt_date.isoformat()+'ALL'
+#f1=open(filename, 'w')
+#f3=open(filename3, 'w')
+#
+#rows = table.find_all('tr')
+##print(rows)
+#for row in rows:
+#    cols = row.findAll('td')
+#    if len(cols) > 0:
+#      country = cols[0].text.strip()
+#      number  = cols[1].text.strip()
+#      diff    = cols[2].text.strip()
+#      np100t  = cols[3].text.strip()
+#      inc7    = cols[4].text.strip()
+#      dead    = cols[5].text.strip()
+#      f1.write("%s, %s, %s, %s, %s, %s\n" % (date, country, number, diff, np100t, dead))
+#      f3.write("%s, %s, %s, %s, %s, %s, %s\n" % (date, country, number, diff, np100t, inc7, dead))
+#    #cols = [ele.text.strip() for ele in cols]
+#    #data.append([ele for ele in cols if ele])
+#
+#f1.close()
+#f3.close()
+#
+#print(filename)
+#f2=open("file_of_the_day", 'w')
+#f2.write("%s\n" % filename)
+#f2.close
